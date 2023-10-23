7b8f82220fea7215ead3b75fb08b087c11c3d700
Yohann Gusso <yohann.gusso@hotmail.com>
2023-10-23 09:38:43 -0300

    commit estrutura

--- calculadora_imc/test/widget_test.dart
+++ calculadora_imc/test/widget_test.dart
@@ -1,30 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:calculadora_imc/main.dart';
+import 'package:calculadora_imc/main.dart'; // Importe o arquivo principal do seu projeto.
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Calculadora de IMC', (WidgetTester tester) async {
+    // Teste para verificar se a calculadora de IMC está funcionando corretamente.
+    // Você pode adicionar seus testes aqui.
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Exemplo de teste para verificar se a interface do usuário contém um botão "Calcular".
+    await tester.pumpWidget(MaterialApp(home: Home()));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final calcularButton = find.widgetWithText(ElevatedButton, 'Calcular');
+    expect(calcularButton, findsOneWidget);
   });
+
+  // Você pode adicionar mais testes conforme necessário.
 }
